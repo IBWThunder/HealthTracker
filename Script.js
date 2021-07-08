127ee54963fa330c30affbc180c2bae68096f281
Khaled Jamal <khaledjamalnaif@gmail.com>
2021-07-08 14:24:56 +0300

    testing out a form

--- Script.js
+++ Script.js
@@ -5,3 +5,10 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+var form = document.getElementById('id01')
+window.onclick = function(event) {
+    if (event.target == form) {
+        form.style.display = "none";
+    }
+}
